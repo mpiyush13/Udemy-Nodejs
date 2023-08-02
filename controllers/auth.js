c5585dd209dd87eae0be5bf15dd965ef88278126
Piyush Maurya <piyushmaurya@Piyushs-MacBook-Air.local>
2023-08-01 23:20:17 -0700

    CRUD operation with session

---
+++ controllers/auth.js
@@ -0,0 +1,29 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById('64c8efb60fd0c236b12ce20a')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
